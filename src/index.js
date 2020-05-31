af274d41199f836a9242f4808b15d2d8ffea0226
George Karametas <georgios.karametas@viooh.com>
2020-05-31 22:39:44 +0100

    Move data outside

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
+const data = [
+  { title: "Harry Potter and the Philosopher's Stone", category: "books" },
+  { title: "Harry Potter and the Chamber of Secrets", category: "books" },
+  { title: "Harry Potter and the Prisoner of Azkaban", category: "books" },
+  { title: "Harry Potter and the Goblet of Fire", category: "books" },
+  { title: "Harry Potter and the Order of the Phoenix", category: "books" },
+  { title: "Harry Potter and the Half-Blood Prince", category: "books" },
+  { title: "Harry Potter and the Deathly Hallows", category: "books" },
+  { title: "History of the world", category: "books" },
+  { title: "Geography", category: "books" },
+  { title: "Animals", category: "books" },
+  { title: "Pottery", category: "books" },
+];
 const search = document.querySelector("input");
 
 search.addEventListener("change", (e) => {
-  const data = [
-    { title: "Harry Potter and the Philosopher's Stone", category: "books" },
-    { title: "Harry Potter and the Chamber of Secrets", category: "books" },
-    { title: "Harry Potter and the Prisoner of Azkaban", category: "books" },
-    { title: "Harry Potter and the Goblet of Fire", category: "books" },
-    { title: "Harry Potter and the Order of the Phoenix", category: "books" },
-    { title: "Harry Potter and the Half-Blood Prince", category: "books" },
-    { title: "Harry Potter and the Deathly Hallows", category: "books" },
-    { title: "History of the world", category: "books" },
-    { title: "Geography", category: "books" },
-    { title: "Animals", category: "books" },
-    { title: "Pottery", category: "books" },
-  ];
   const r = document.getElementById("results");
   if (r) r.remove();
 
